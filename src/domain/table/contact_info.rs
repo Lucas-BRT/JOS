81c21fba10258efbb930dfde811365cd3208de06
lucasBRT <lucasbritodepaula.t@gmail.com>
2025-05-24 15:04:28 -0300

    add contact info for table

--- src/domain/table/contact_info.rs
+++ src/domain/table/contact_info.rs
@@ -1,10 +1,11 @@
 use serde::{Deserialize, Serialize};
 
-use crate::domain::email::Email;
+use crate::domain::{email::Email, phone_number::PhoneNumber};
 
 #[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
 pub enum ContactInfo {
     Email(Email),
-    Phone(String),
-    Address(String),
+    Phone(PhoneNumber),
+    // TODO: Implement other contact info types and validate the size of the string
+    Other(String),
 }
--- src/domain/table/table.rs
+++ src/domain/table/table.rs
@@ -2,16 +2,16 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-use super::{description::Description, title::Title};
+use super::{contact_info::ContactInfo, description::Description, title::Title};
 
 #[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
-pub struct TableRow {
+pub struct Table {
     pub id: Uuid,
     pub gm_id: Uuid,
     pub title: Title,
     pub description: Option<Description>,
     pub system_id: u32,
-    pub contact_info: String,
+    pub contact_info: ContactInfo,
     pub max_players: Option<u32>,
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
